bae2ad94c42bc063eb696e8bc8b8793d8616760e
chopin-coding <emreerguvan@gmail.com>
2023-10-19 17:46:26 +0400

    nada

--- static/src/ts/components/App.vue
+++ static/src/ts/components/App.vue
@@ -1,40 +1,44 @@
 <script setup lang="ts">
-import {ref, watch} from "vue";
+import { ref, watch } from "vue";
 import * as treeHelpers from "../treeHelpers";
-import {useLocalStorage} from "@vueuse/core";
+import { useLocalStorage } from "@vueuse/core";
 import TreeComponent from "./TreeComponent.vue";
 import TreeListComponent from "./TreeListComponent.vue";
 
-
 // FIXME: loggedIn and userTrees error handling
-const loggedIn: boolean = JSON.parse(document.getElementById("logged-in").textContent);
+const loggedIn: boolean = JSON.parse(
+  document.getElementById("logged-in").textContent,
+);
 
-
-const localTreeStore = useLocalStorage('tree-store', {trees: [treeHelpers.defaultTree()]})
-let syncTimerBaseCount = 3000
+const localTreeStore = useLocalStorage("tree-store", {
+  trees: [treeHelpers.defaultTree()],
+});
+let syncTimerBaseCount = 3000;
 
 let tempTreeStore = ref([]);
 let syncTimer = null;
-let syncIndicator = ref('synced')
+let syncIndicator = ref("synced");
 let selectedTreeIndex = ref(0);
 
-initializeTrees()
+initializeTrees();
 
 function initializeTrees() {
   if (loggedIn) {
-    tempTreeStore.value = JSON.parse(document.getElementById("user-trees").textContent)
+    tempTreeStore.value = JSON.parse(
+      document.getElementById("user-trees").textContent,
+    );
 
-    initializeTreeWatchers()
+    initializeTreeWatchers();
   } else {
+    // syncTimerBaseCount = 1000;
     // TODO: see if this could cause performance issues
-    tempTreeStore.value = localTreeStore.value.trees
+    tempTreeStore.value = localTreeStore.value.trees;
   }
 }
 
-
 function treeWatcher(treeIndex: string) {
   watch(tempTreeStore.value[treeIndex], (newValue, oldValue) => {
-    syncIndicator.value = "syncing"
+    syncIndicator.value = "syncing";
 
     // reset the sync timer
     if (syncTimer) {
@@ -43,38 +47,39 @@ function treeWatcher(treeIndex: string) {
 
     // start the sync timer
     syncTimer = setTimeout(async () => {
-      const updateStatus = await treeHelpers.updateTree(tempTreeStore.value[treeIndex], loggedIn)
+      const updateStatus = await treeHelpers.updateTree(
+        tempTreeStore.value[treeIndex],
+        loggedIn,
+      );
 
       if (updateStatus === 200) {
-        syncIndicator.value = "synced"
+        syncIndicator.value = "synced";
       } else {
-        syncIndicator.value = "failed"
+        syncIndicator.value = "failed";
       }
-
     }, syncTimerBaseCount);
   });
 }
 
 function initializeTreeWatchers() {
   for (const index in tempTreeStore.value) {
-    treeWatcher(index)
+    treeWatcher(index);
   }
 }
 
 function addTreeWatcher(treeId: string) {
   const indexToWatch = tempTreeStore.value.findIndex(
-      (tree) => tree.tree_id === treeId,
+    (tree) => tree.tree_id === treeId,
   );
 
   if (indexToWatch !== -1) {
-    treeWatcher(indexToWatch.toString())
+    treeWatcher(indexToWatch.toString());
   }
 }
 
-
 function selectTreeHandler(treeId: string): void {
   const indexToSelect = tempTreeStore.value.findIndex(
-      (tree) => tree.tree_id === treeId,
+    (tree) => tree.tree_id === treeId,
   );
 
   if (indexToSelect !== -1) {
@@ -89,33 +94,31 @@ function selectTreeHandler(treeId: string): void {
   <div>
     <div>
       <div>
-        <span v-text="syncIndicator">
-
-        </span>
+        <span v-text="syncIndicator"> </span>
       </div>
       <div>
         <label for="selected-tree-name-input">Tree Name</label>
         <input
-            id="selected-tree-name-input"
-            v-model="tempTreeStore[selectedTreeIndex].tree_name"
+          id="selected-tree-name-input"
+          v-model="tempTreeStore[selectedTreeIndex].tree_name"
         />
       </div>
       <component
-          v-if="tempTreeStore.length !== 0"
-          :is="TreeComponent"
-          :tree-nodes="tempTreeStore[selectedTreeIndex].tree_data"
-          :logged-in="loggedIn"
+        v-if="tempTreeStore.length !== 0"
+        :is="TreeComponent"
+        :tree-nodes="tempTreeStore[selectedTreeIndex].tree_data"
+        :logged-in="loggedIn"
       />
       <div v-else>No trees to show</div>
     </div>
 
     <div>
       <component
-          :is="TreeListComponent"
-          :trees="tempTreeStore"
-          :logged-in="loggedIn"
-          @select-tree="(treeId: string) => selectTreeHandler(treeId)"
-          @create-tree="(treeId: string) => addTreeWatcher(treeId)"
+        :is="TreeListComponent"
+        :trees="tempTreeStore"
+        :logged-in="loggedIn"
+        @select-tree="(treeId: string) => selectTreeHandler(treeId)"
+        @create-tree="(treeId: string) => addTreeWatcher(treeId)"
       />
     </div>
   </div>
