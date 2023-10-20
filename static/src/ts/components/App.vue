4fe8c4a03006ac3c91b926286fe73b9c76a3d526
chopin-coding <emreerguvan@gmail.com>
2023-10-20 23:57:03 +0400

    tailwind custom color palette

--- static/src/ts/components/App.vue
+++ static/src/ts/components/App.vue
@@ -91,9 +91,6 @@ function selectTreeHandler(treeId: string): void {
 </script>
 
 <template>
-  <div class="text-3xl font-bold underline text-sky-400">
-    test tailwind
-  </div>
   <div>
     <div>
       <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.{vue,js,ts,jsx,tsx,html,css}", "./**/templates/**/*.{vue,js,ts,jsx,tsx,html,css}", "./static/src/**/*.{vue,js,ts,jsx,tsx,html,css}"],
-    theme: {
-        extend: {},
+  darkMode: "class",
+  content: [
+    "./templates/*/*.{vue,ts,html,css}",
+    "./anxiety/templates/*/*.{vue,ts,html,css}",
+    "./static/src/*/*.{vue,ts,html,css}",
+  ],
+  theme: {
+    colors: {
+      textwhite: "rgb(var(--color-textwhite) / 1)",
+      textwhitedim: "rgb(var(--color-textwhite) / .8)",
+      textblack: "rgb(var(--color-textblack) / 1)",
+      backg: "rgb(var(--color-backg) / 1)",
+      primary: "rgb(var(--color-primary) / 1)",
+      primaryshade: "rgb(var(--color-primary) / .8)",
+      secondary: "rgb(var(--color-secondary) / 1)",
+      accent: "rgb(var(--color-accent) / 1)",
+      accentshade: "rgb(var(--color-accent) / .2)",
     },
-    plugins: [],
-}
+    fontFamily: {
+      sans: ["Lato", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+// @import url('https://fonts.googleapis.com/css?family=Lato:700|Lato:400');
+//
+// body {
+//   font-family: 'Lato';
+//   font-weight: 400;
+// }
+//
+// h1, h2, h3, h4, h5 {
+//   font-family: 'Lato';
+//   font-weight: 700;
+// }
+//
+// html {font-size: 100%;} /* 16px */
+//
+// h1 {font-size: 3.053rem; /* 48.8px */}
+//
+// h2 {font-size: 2.442rem; /* 39.04px */}
+//
+// h3 {font-size: 1.954rem; /* 31.2px */}
+//
+// h4 {font-size: 1.563rem; /* 24.96px */}
+//
+// h5 {font-size: 1.250rem; /* 20px */}
+//
+// small {font-size: 0.800rem; /* 12.8px */}
