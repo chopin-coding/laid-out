dc9b44a570580a6797d1a76b99310e73c1da37fa
chopin-coding <emreerguvan@gmail.com>
2023-10-23 00:04:43 +0400

    small styling fixes

--- static/src/ts/components/TreeListComponent.vue
+++ static/src/ts/components/TreeListComponent.vue
@@ -41,7 +41,7 @@ async function deleteBtnHandler(treeId: string, loggedIn: boolean) {
 
 <template>
   <div>
-    <div class="text-textblackdim text-xl">Trees</div>
+    <div class="text-xl text-textblackdim">Trees</div>
     <ul class="list-none">
       <li v-for="tree in trees" :key="tree.tree_id">
         <a href="#" v-text="tree.tree_name"
@@ -49,10 +49,10 @@ async function deleteBtnHandler(treeId: string, loggedIn: boolean) {
 
         </a>
         <button class="text-textblackdim" v-show="trees.length > 1" @click="deleteBtnHandler(tree.tree_id, loggedIn)">
-          <svg class="h-4 w-4 " viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="m18 6-.8 12.013c-.071 1.052-.106 1.578-.333 1.977a2 2 0 0 1-.866.81c-.413.2-.94.2-1.995.2H9.994c-1.055 0-1.582 0-1.995-.2a2 2 0 0 1-.866-.81c-.227-.399-.262-.925-.332-1.977L6 6M4 6h16m-4 0-.27-.812c-.263-.787-.394-1.18-.637-1.471a2 2 0 0 0-.803-.578C13.938 3 13.524 3 12.694 3h-1.388c-.829 0-1.244 0-1.596.139a2 2 0 0 0-.803.578c-.243.29-.374.684-.636 1.471L8 6"
-                stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         </button>
       </li>
