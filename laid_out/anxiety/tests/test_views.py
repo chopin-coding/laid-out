594ddcfa13226fd20e1b811d8bc76578672edd2a
chopin-coding <emreerguvan@gmail.com>
2023-12-13 23:39:55 +0400

    refactor anxiety view tests

--- laid_out/anxiety/tests/test_views.py
+++ laid_out/anxiety/tests/test_views.py
@@ -1,10 +1,8 @@
 import pytest
-from django.contrib.auth.models import AnonymousUser
-from django.test import Client, RequestFactory
+from django.test import Client
 from django.urls import reverse
 
 from laid_out.anxiety.models import AnxietyTree
-from laid_out.anxiety.views import anxiety_view
 from laid_out.users.models import User
 
 pytestmark = [pytest.mark.django_db, pytest.mark.unit]
@@ -35,28 +33,26 @@ class TestAnxietyPage:
         assert not response.context["logged_in"]
         assert not response.context["user_trees"]
 
-    def test_anxiety_view_authenticated_user(self, user: User, rf: RequestFactory):
-        rf.user = user
-
+    def test_anxiety_view_authenticated_user(self, user: User, client: Client):
         # a new user doesn't have any anxiety trees
         assert user.anxiety_trees.all().count() == 0
 
-        anxiety_view(request=rf)
+        client.force_login(user=user)
+
+        response = client.get(reverse("anxiety:home"))
+        assert response is not None
 
         # the user is automatically assigned a tree after navigating to the anxiety page
         assert user.anxiety_trees.all().count() == 1
 
-        anxiety_view(request=rf)
-
+        response = client.get(reverse("anxiety:home"))
+        assert response is not None
         # the user is not automatically assigned a second tree because they already have one
         assert user.anxiety_trees.all().count() == 1
 
-    def test_anxiety_view_anonymous_user(self, user: User, rf: RequestFactory):
+    def test_anxiety_view_anonymous_user(self, client: Client):
         number_of_anxiety_trees = AnxietyTree.objects.all().count()
+        response = client.get(reverse("anxiety:home"))
 
-        rf.user = AnonymousUser()
-
-        anxiety_view(request=rf)
-
-        # no new anxiety trees are created for after an anonymous user has navigated to the anxiety page
+        assert response is not None
         assert AnxietyTree.objects.all().count() == number_of_anxiety_trees
