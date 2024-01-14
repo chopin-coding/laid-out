4fdae6ee1a3d16ac78017b393505b3e498f2a5fb
chopin-coding <emreerguvan@gmail.com>
2024-01-14 17:16:57 +0400

    fix footer mobile visual issue

--- laid_out/anxiety/tests/test_views.py
+++ laid_out/anxiety/tests/test_views.py
@@ -18,7 +18,7 @@ class TestAnxietyPage:
         assert response.context["user_trees"] is not None
         assert response.context["user_trees"][0]["tree_name"] == "New Tree"
 
-        assert response.context["ANXIETY_API_BASE_URL"] is not None
+        assert response.context["ANXIETY_API_BASE_URL"] != ""
 
     def test_anxiety_view_context_authenticated_user_with_trees(self, user: User, client: Client):
         client.force_login(user=user)
