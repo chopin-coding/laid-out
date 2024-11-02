4650cf07c6c0863f4fe8916cafff050f47b4dcf0
chopin-coding <emreerguvan@gmail.com>
2024-11-02 15:57:23 +0400

    remove docs

--- config/settings/production.py
+++ config/settings/production.py
@@ -236,8 +236,6 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 # http://whitenoise.evans.io/en/stable/django.html#WHITENOISE_IMMUTABLE_FILE_TEST
-
-
 def immutable_file_test(path, url):
     # Match vite (rollup)-generated hashes, à la, `some_file-CSliV9zW.js`
     return re.match(r"^.+[.-][0-9a-zA-Z_-]{8,12}\..+$", url)
